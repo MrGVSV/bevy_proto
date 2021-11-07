d279aff338fef47e6df5be5c9645325584f37976
MrGVSV <gino.valente.code@gmail.com>
2021-11-07 00:48:24 -0700

    Import/re-export bevy_proto_derive

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+extern crate bevy_proto_derive;
 mod components;
 mod data;
 mod plugin;
 mod prototype;
 
+pub use bevy_proto_derive::*;
 pub use components::*;
 pub use data::*;
 pub use plugin::*;
