66b0da79ab59bd387f7180d3cd749c441e393bf7
Gino Valente <gino.valente.code@gmail.com>
2022-03-28 21:46:05 -0700

    Updated docs for derive macro

--- bevy_proto_derive/src/lib.rs
+++ bevy_proto_derive/src/lib.rs
@@ -10,14 +10,51 @@ mod attributes;
 /// Automatically implements [`ProtoComponent`] for the given
 /// struct or enum. This works on all structs and enums, including tuple and unit structs.
 ///
-/// **NOTE: `Clone`, `serde::Serialize`, and `serde::Deserialize` must also be implemented/derived**
+/// > NOTE: At minimum `Reflect` and `FromReflect` must also be derived/implemented. Depending
+/// > on attribute usage, `Clone` and `Component` will also likely need to be derived/implemented.
+///
+/// # Container Attributes
+///
+/// These are attributes that can be applied to the container type.
+///
+/// * `#[proto_comp(into = "SomeComponent")]`
+///    
+///   This will convert this `ProtoComponent` into the given component using `.into()`
+///   when inserting into the entity. The type must be in scope and must implement
+///   `Component`. This requires that `Clone` also be derived/implemented.
+///
+/// * `#[proto_comp(into_bundle = "SomeBundle")]`
+///
+///   This will convert this `ProtoComponent` into the given bundle using `.into()`
+///   when inserting into the entity. The type must be in scope and must implement
+///   `Bundle`. This requires that `Clone` also be derived/implemented.
+///
+/// * `#[proto_comp(with = "some_func")]`
+///
+///   This will pass a reference to this `ProtoComponent` and a mutable reference
+///   to the entity to the given function. The function must take the form:
+///   `fn some_func(component: &MyComponent, entity: &mut EntityMut)` and it must
+///   be in scope. This function will be called when inserting into the entity.
+///
+/// # Field Attributes
+///
+/// * `#[proto_comp(preload(type = "Image"))]`
+///
+///   This will mark this field to be used during the asset preloading phase. The
+///   given `type` must correspond to a valid asset type and it must be in scope.
+///
+///   Additionally, you can specify a `dest` with the name (or index) of the field
+///   to insert the strong handle into. If no `dest` is set, this asset will be
+///   loaded as a dependency of the prototype. Some examples:
+///
+///   - `#[proto_comp(preload(type = "Image", dest = "my_handle"))]`
+///   - `#[proto_comp(preload(type = "Mesh", dest = "1"))]`
 ///
 /// # Examples
 ///
-/// ```
-/// # use serde::{Deserialize, Serialize};
-///
-/// #[derive(Clone, Serialize, Deserialize, ProtoComponent)]
+/// ```ignore
+/// #[derive(Reflect, FromReflect, Component, ProtoComponent, Clone)]
+/// #[reflect(ProtoComponent)]
 /// struct SomeComponent {
 ///     some_string: String,
 ///     some_int: i32,
@@ -25,17 +62,17 @@ mod attributes;
 ///
 /// // Which generates:
 /// //
-/// // #[typetag::serde]
-/// // impl bevy_proto::ProtoComponent for #ident { ///
-/// // 	fn insert_self(
-/// //    &self,
-/// //    commands: &mut bevy_proto::ProtoCommands,
-/// // 	  asset_server: &bevy::prelude::Res<bevy::prelude::AssetServer>,
-/// //  ) {
-/// //      let component = self.clone();
-/// //      commands.insert(component);
-/// //    }
-/// //  }
+/// // impl ::bevy_proto::prelude::ProtoComponent for SomeComponent {
+/// //   fn apply(&self, entity: &mut bevy::ecs::world::EntityMut) {
+/// //     entity.insert(self.clone());
+/// //   }
+/// //   fn as_reflect(&self) -> &dyn bevy::prelude::Reflect {
+/// //     self
+/// //   }
+/// //   fn preload_assets(&mut self, preloader: &mut ::bevy_proto::prelude::AssetPreloader) {
+/// //     use ::bevy_proto::prelude::StoreHandle;
+/// //   }
+/// // }
 /// ```
 #[proc_macro_derive(ProtoComponent, attributes(proto_comp))]
 pub fn proto_comp_derive(input: TokenStream) -> TokenStream {
