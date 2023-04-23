d19e7e8e616288a0fe1eb88dfd21bd7afe5f9a9e
Gino Valente <gino.valente.code@gmail.com>
2023-04-23 14:50:45 -0700

    Fix derive macro relying on a bevy_proto_backend dependency

--- bevy_proto_derive/src/utils.rs
+++ bevy_proto_derive/src/utils.rs
@@ -1,12 +1,20 @@
 use proc_macro2::{Ident, Span, TokenStream};
-use proc_macro_crate::{crate_name, FoundCrate};
+use proc_macro_crate::{crate_name, Error, FoundCrate};
 use quote::quote;
 
-/// Returns a path to the `bevy_proto_backend` crate.
+/// Returns a path to either the `bevy_proto_backend` crate or `bevy_proto::backend` module.
 pub(crate) fn get_proto_crate() -> TokenStream {
-    get_crate_path(
-        crate_name("bevy_proto_backend").expect("bevy_proto_backend is present in `Cargo.toml`"),
-    )
+    match crate_name("bevy_proto_backend") {
+        Ok(found_crate) => get_crate_path(found_crate),
+        Err(Error::CrateNotFound { .. }) => {
+            let path = get_crate_path(
+                crate_name("bevy_proto")
+                    .expect("bevy_proto or bevy_proto_backend should be present in `Cargo.toml`"),
+            );
+            quote!(#path::backend)
+        }
+        Err(error) => panic!("{}", error),
+    }
 }
 
 /// Returns a path to the `bevy` crate.
@@ -19,7 +27,7 @@ fn get_crate_path(found_crate: FoundCrate) -> TokenStream {
         FoundCrate::Itself => quote!(crate),
         FoundCrate::Name(name) => {
             let ident = Ident::new(&name, Span::call_site());
-            quote!( #ident )
+            quote!( ::#ident )
         }
     }
 }
